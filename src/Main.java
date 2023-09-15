29b14863a785b2a1732d73d159df20a87ac57616
eghearn <eghearn05@gmail.com>
2023-09-14 23:16:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int numberToExamine = 3;
+        double evenOrOdd = numberToExamine % 2;
+        System.out.println("Your number is odd: " + evenOrOdd);
+    }
+}
